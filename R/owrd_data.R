126b3b14363778c43c5116d03c8ab6b244fa2e57
Travis Pritchard <pritchard.travis@deq.state.or.us>
2021-05-20 10:01:40 -0700

    Add OWRD functions

---
+++ R/owrd_data.R
@@ -0,0 +1,90 @@
+
+#' Retrieve surface water monitoring results from Oregon Water Resources Department
+#'
+#' Data is retrieved from Oregon Water Resources Department's near real time monitoring results.
+#' https://apps.wrd.state.or.us/apps/sw/hydro_near_real_time/
+#'
+#' @param station Required string vector of stations to be fetched.
+#' @param startdate Required string setting the start date of the data being fetched. Format 'yyyy-mm-dd'.
+#' @param enddate Required string setting the end date of the data being fetched. Format 'yyyy-mm-dd'.
+#' @param char Required string vector identifying the characteristics to be fetched. Options include:
+#'   * 'MDF' - Mean Daily Flow
+#'   * 'Instantaneous_Stage' - Instantaneous Stage
+#'   * 'Instantaneous_Flow' - Instantaneous Flow
+#'   * 'Measurements' - Discharge grab measurements
+#'   * 'WTEMP_MEASURE' - Water temperature grab measurements
+#'   * 'WTEMP15' - Instantaneous Water temperature
+#'   * 'WTEMP_MEAN' - Daily mean Water temperature
+#'   * 'WTEMP_MAX' - Daily maximum Water temperature
+#'   * 'WTEMP_MIN' - Daily minimum Water temperature
+#' @export
+#' @return data frame of data
+
+owrd_data <- function(station, startdate, enddate, char) {
+
+  # Testing
+  # station=c("10366000", "13330000")
+  # startdate="2020-12-01"
+  # enddate="2021-02-15"
+  # char=c("MDF", "WTEMP_MAX")
+
+  WRDurl <- "https://apps.wrd.state.or.us/apps/sw/hydro_near_real_time/hydro_download.aspx?"
+
+  if(!any(char %in% c('WTEMP15', 'WTEMP_MEASURE', 'WTEMP_MEAN', 'WTEMP_MAX', 'WTEMP_MIN, MDF', 'Instantaneous_Stage', 'Instantaneous_Flow', 'Measurements'))) {
+    stop("non valid value in 'char'")
+  }
+
+  df.query <- expand.grid(WRDurl=WRDurl, station=station, startdate=startdate, enddate=enddate, char=char)
+
+  df.query <- df.query %>%
+    dplyr::mutate(query=dplyr::case_when(char %in% c('MDF', 'Instantaneous_Stage', 'Instantaneous_Flow', 'Measurements') ~
+                                           paste0(WRDurl,
+                                                  "station_nbr=", station,
+                                                  "&start_date=", startdate,
+                                                  "%2012:00:00%20AM&",
+                                                  "end_date=", enddate ,
+                                                  "%2012:00:00%20AM&",
+                                                  "dataset=", char,
+                                                  "&format=tab"),
+                                         TRUE ~
+                                           paste0(WRDurl, "station_nbr=",station,
+                                                  "&start_date=",startdate ,
+                                                  "%2012:00:00%20AM&",
+                                                  "end_date=",enddate ,
+                                                  "%2012:00:00%20AM&",
+                                                  "dataset=",char ,
+                                                  "&format=tab&units=C")))
+
+
+  df1 <- df.query %>%
+    dplyr::group_by(query) %>%
+    dplyr::group_split() %>%
+    lapply(FUN = RCurl::getURL) %>%
+    lapply(FUN = textConnection) %>%
+    lapply(FUN=read.table, header = TRUE, sep ="\t", skip=0, stringsAsFactors=FALSE)
+
+  # remove . from column names
+  df2 <- lapply(df1, FUN=function(x){
+    colnames(x) <- gsub(x=colnames(x), pattern="\\.", replacement="")
+    return(x)})
+
+  # pivot the data into long format
+  df3 <- lapply(df2, tidyr::pivot_longer, cols=dplyr::any_of(c("mean_daily_flow_cfs", "instananteous_stage_ft", "instananteous_flow_cfs", "measured_stage_ft",
+                                                               "temperature_measurement_C", "instantaneous_water_temp_C", "daily_mean_water_temp_C",
+                                                               "daily_max_water_temp_C", "daily_min_water_temp_C",
+                                                               "temperature_measurement_F", "instantaneous_water_temp_F", "daily_mean_water_temp_F",
+                                                               "daily_max_water_temp_F", "daily_min_water_temp_F")), values_to = "Result.Value", names_to = "Characteristic.Name")
+
+  # fix various data types and add cols when they don't exist
+  df4 <- df3 %>%
+    lapply(FUN=dplyr::mutate, time_observed=if(!"time_observed" %in% colnames(.)) as.character(NA)) %>%
+    lapply(FUN=dplyr::mutate, download_date=if(!"download_date" %in% colnames(.)) as.character(NA)) %>%
+    lapply(FUN=dplyr::mutate, record_date=as.character(record_date)) %>%
+    lapply(FUN=dplyr::mutate, published_status=as.character(published_status)) %>%
+    lapply(FUN=dplyr::mutate, download_date=as.character(download_date)) %>%
+    dplyr::bind_rows() %>%
+    dplyr::select(station_nbr, record_date, time_observed, Characteristic.Name, Result.Value, published_status, download_date) #estimated, revised,
+
+  return(df4)
+
+}
--- usage.R
+++ usage.R
@@ -87,3 +87,90 @@ data_split_AWQMS(pH_cont, split_on = "Monitoring_Location_ID", size = 100000,
 write.csv(pH_deployments, file = '//deqlab1/Assessment/Integrated_Report/DataSources/2022/City of portland- continuous/pH_deployments.csv',
           row.names = FALSE)
 
+
+
+
+# OWRD  -------------------------------------------------------------------------------------------------------
+
+#
+station=c("14335500","14342500","14104800",
+          "14335200", "14335230","14335300","14343000",
+          "14346700","14354100","14347800","14336700",
+          "14337000","14368300")
+startdate="2016-01-01"
+enddate="2020-12-31"
+char=c("WTEMP_MAX")
+
+a <- owrd_data(station, startdate, enddate, char)
+
+
+station =c("14104190","14032400","14031050","14357503",
+           "14021000","14358725","14348080","14335235",
+           "14348000","14335250","14320700","14341610",
+           "14348150","14352001","14355875","14358750",
+           "14022500","14023500","14350900","14352000",
+           "14025000","14026000","14365500","14363450",
+           "14360500","14340800","14357000","14354950",
+           "14327122","14192500","14358800","13330500",
+           "14358680")
+
+b <- owrd_data(station, startdate, enddate, char)
+
+station =c("14306900","14193000","11495900","11494000",
+           "14350000","14076100","14029900","14348400",
+           "11504109","14327137","11510000","13330300",
+           "14024100","14063000","14081500","13275105",
+           "14024300","14060000","14358610","14088500",
+           "14079800","14083400","14085700","11495600",
+           "13275300","14073520","14075000","14087300",
+           "14082550","14375200","11504040","11504103",
+           "13329100","13216500")
+
+c <- owrd_data(station, startdate, enddate, char)
+
+station =c("14095250","14095255","14306820","14080500",
+           "14400200","14202850","13273000","14202510",
+           "14054000","14076020","11497100","14074900",
+           "14070920","13331450","13330000","14070980",
+           "13325500","14327120","11502550","13214000",
+           "13269450","11500400","13317850","13329765",
+           "13215000","13217500","11504120","14346900",
+           "11497550","11500500","11494510","11497500",
+           "14064500","14056500","14073000","13318210",
+           "11491400","13282550","13318060","13318920",
+           "14039500","10392400","14327300","11503500",
+           "14031600","14010800","14032000","14105550",
+           "14104700","14105545")
+
+d <- owrd_data(station, startdate, enddate, char)
+
+all <- rbind(a,b,c,d)
+
+all_sum <- all %>%
+  group_by(published_status) %>%
+  summarise(n= n(),
+            per_status = round(((n/205595)*100),digits = 2))
+
+# Plot percentages
+ggplot(data=all_sum, aes(x=published_status, y=per_status)) +
+  geom_bar(stat="identity", fill="steelblue")+
+  geom_text(aes(label=per_status), vjust=-0.3, size=3.5)+
+  theme_minimal()
+
+all_pub <- all %>%
+  filter(published_status == 'PUB')
+
+stations_pub <- all_pub %>%
+  select(station_nbr) %>%
+  distinct()
+
+save.image(file = "//deqlab1/Assessment/Integrated_Report/DataSources/2022/OWRD/all_data.RData")
+
+
+#format to AWQMS
+
+OWRD_temp_sumstats <- owrd_AWQMS(all_pub, project = 'Integrated Report – Call for Data')
+
+
+data_split_AWQMS(OWRD_temp_sumstats, split_on = "SiteID", size = 100000,
+                 filepath = '//deqlab1/Assessment/Integrated_Report/DataSources/2022/OWRD/')
